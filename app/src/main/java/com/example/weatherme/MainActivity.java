8a7917a1a220e4b3a8f510fd8258a3b4f581d12f
programmingalex2019 <programmingalex2019@gmail.com>
2022-01-16 14:50:39 +0200

    API does not provide weather by city -> -> removed editText for city
    Get coordinates and fetch API data works.
    Landing screen finished.
    
    TODO:
    
    implement Logging and Registration Screens

--- app/src/main/java/com/example/weatherme/MainActivity.java
+++ app/src/main/java/com/example/weatherme/MainActivity.java
@@ -1,59 +1,120 @@
 package com.example.weatherme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
-    private TextInputLayout cityInput;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    //fields
+    private static String API_KEY = "47f309e7-d912-4d21-baf7-5e19628781f0";
     private TextView cityCountry;
-    private TextView temperature;
-    private Button findButton;
+    private TextView cityTemperature;
+    private LocationManager locationManager;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cityInput = findViewById(R.id.cityInput);
+        //initialize fields
         cityCountry = findViewById(R.id.cityCountry);
-        findButton = findViewById(R.id.findCity);
+        cityTemperature = findViewById(R.id.cityDegrees);
+        //initialize queue for requests
+        mQueue = Volley.newRequestQueue(this);
 
-        findButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(!validateCity()){
-                    return;
-                }
-
-                cityCountry.setText(cityInput.getEditText().getText().toString());
-
-            }
-        });
-
-    }
-
-    private boolean validateCity(){
-
-        String city = cityInput.getEditText().getText().toString().trim();
-
-        if(city.isEmpty()){
-            cityInput.setError("Field can't be empty");
-            cityInput.setErrorEnabled(true);
-            return false;
-        }else{
-            cityInput.setError(null);
-            cityInput.setErrorEnabled(false);
-            return true;
+        //runtime permission
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.ACCESS_FINE_LOCATION},100);
         }
+
+        //access latitude and longitude -> request for weather -> update UI
+        getLocationAndWeather();
+
     }
 
+    @SuppressLint("MissingPermission")
+    private void getLocationAndWeather(){
+
+        try{
+
+            //access android resources
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            //store coordinates
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+
+            //request details string
+            String urlString = "https://api.airvisual.com/v2/nearest_city?lat="+ latitude +"&lon="+ longitude +"&key="+ API_KEY;
+
+            //JSON GET REQUEST -> using Volley library
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, urlString, null,
+                    new Response.Listener<JSONObject>(){
+                        @Override
+                        public void onResponse(JSONObject response){
+                            try{
+
+                                JSONObject jsonObject = response.getJSONObject("data"); //main object
+                                //access individual objects and json fields
+                                cityCountry.setText(String.format("%s,%s",jsonObject.getString("city"),  jsonObject.getString("country")));
+                                cityTemperature.setText(String.format("%s°C",jsonObject.getJSONObject("current").getJSONObject("weather").getString("tp")));
+
+                            }catch (JSONException e){
+                                e.printStackTrace(); //prints all errors
+                            }
+                        }
+
+                    }, new Response.ErrorListener(){
+
+                @Override
+                public void onErrorResponse(VolleyError error){
+                    error.printStackTrace(); //prints all errors
+                }
+            });
+
+            mQueue.add(request); //add request to queue
+
+
+        }catch(Exception e){
+            e.printStackTrace(); //prints all errors
+        }
+
+    }
+
+    // must have due to interface implementation //not used
+    @Override
+    public void onLocationChanged(Location location) {}
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {}
+
+    @Override
+    public void onProviderEnabled(String s) {}
+
+    @Override
+    public void onProviderDisabled(String s) {}
 }
