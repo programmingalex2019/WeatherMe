2389e90da5cfddbb237f6c4fd974f9e62c9397f2
programmingalex2019 <programmingalex2019@gmail.com>
2022-01-17 20:14:56 +0200

    Registration and LogIn with Firebase added. CurrentUser and isLogged in added.
    TODO:
    implement home screen and sign out functionality

--- app/src/main/java/com/example/weatherme/SignIn.java
+++ app/src/main/java/com/example/weatherme/SignIn.java
@@ -6,14 +6,21 @@ import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignIn extends AppCompatActivity {
 
@@ -24,19 +31,26 @@ public class SignIn extends AppCompatActivity {
     private TextView switchSignIn;
     private static boolean signInToggle = false; //False for logIn - True for register
 
+    private FirebaseAuth mAuth;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_layout);
 
+        //Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        //credentials editText
         textInputEmail = findViewById(R.id.emailInput);
         textInputPassword = findViewById(R.id.passwordInput);
 
+        //toggle fields
         textLogo = findViewById(R.id.textLogo);
         signButton = findViewById(R.id.signButton);
-
         switchSignIn = findViewById(R.id.switchSignIn);
 
+        //toggle between register and login UI
         switchSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,9 +70,19 @@ public class SignIn extends AppCompatActivity {
             }
         });
 
-
     }
 
+    //check if user logged in
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //change Activity with
+            Toast.makeText(SignIn.this, "Current User success: " + currentUser.getEmail(), Toast.LENGTH_SHORT).show();
+        }
+    }
 
     private boolean validateEmail(){
         String emailInput = textInputEmail.getEditText().getText().toString().trim();
@@ -93,17 +117,70 @@ public class SignIn extends AppCompatActivity {
         }
     }
 
-    private void confirmInput(View v){
+    public void confirmInput(View v){
 
         if(!validateEmail() | !validatePassword()){
             return;
         }
 
-        String input = "Email: " + textInputEmail.getEditText().getText().toString().trim();
+        String email = textInputEmail.getEditText().getText().toString().trim();
+        String password = textInputPassword.getEditText().getText().toString().trim();
+
+        String input = "Email: " + email;
         input += "\n";
-        input += textInputPassword.getEditText().getText().toString().trim();
+        input += password;
 
         Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
 
+        // login / register
+        if(!signInToggle){
+            signInUserWithEmailAndPassword(email, password);
+        }else{
+            createUserWithEmailAndPassword(email,password);
+        }
+
     }
+
+    private void createUserWithEmailAndPassword(String email, String password){
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("SignUp", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //update UI with User
+                            Toast.makeText(SignIn.this, "Authentication Registration success with user: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                        }else{
+                            // If sign in fails, display a message to the user.
+                            Log.w("SignUp", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignIn.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void signInUserWithEmailAndPassword(String email, String password){
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("SignIn", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //update UI with User
+                            Toast.makeText(SignIn.this, "Authentication LogIn success with user: " + user.getEmail(), Toast.LENGTH_SHORT).show();
+                        }else{
+                            // If sign in fails, display a message to the user.
+                            Log.w("SignIn", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(SignIn.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
 }
